0c25124bd33fcfbbe0447e1ce6bd41f12f88d13c
soso01 <soso01dev@gmail.com>
2023-02-26 15:15:39 +0900

    <feat> server my-lib 적용 및 패키지 빌드

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { add } from '@packages/my-lib';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Hello World! - (${add(10, 20)})`;
   }
 }
